ab42b62ea3b9994ebd93dc945044755243c469a2
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2023-09-29 17:18:34 -0400

    adding stuff to main homepage

--- crunchkiosk/src/pages/Home.js
+++ crunchkiosk/src/pages/Home.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import '../css/Home.css';
+import Navbar from '../components/Navbar';
 
 
 const Home = () => {
   return (
-
     <>
+    <Navbar/>
+    {/* <div className="pricing_budget">
+        <div className="pricingTitle">
+            Pricing Tier:
+        </div>
+
+        <div className='pricingSelect'>
+            <select>
+                <option value="tier1">Member</option>
+                <option value="tier2">Non-Member</option>
+            </select>
+        </div>
+
+        <div className="budgetTitle">
+            Budget:
+        </div>
+
+        <input className='budgetVal'/>
+    </div> */}
+
     </>
   );
 };
