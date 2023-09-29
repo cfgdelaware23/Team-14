050f8a84c7d93d3d40984ee21de508fe594d95e9
Abhi <abhijit.das3002@gmail.com>
2023-09-29 17:28:51 -0400

    Added page routing

--- crunchkiosk/src/index.js
+++ crunchkiosk/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AutoCartPage from './autoCartPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/autoCartPage" element={<AutoCartPage />} />
+  </Routes>
+  </BrowserRouter>
+
 );
 
 
