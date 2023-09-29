50f7cc19276a895df919f19dfd8408f5a11e7f7e
Timothy <timkhumpan@gmail.com>
2023-09-29 19:18:27 -0400

    update Home

--- crunchkiosk/src/pages/Home.js
+++ crunchkiosk/src/pages/Home.js
@@ -7,65 +7,68 @@ const Home = () => {
   const [glutenFree, setGlutenFree] = useState("");
   const [sugarFree, setSugarFree] = useState("");
   const [dairyFree, setDairyFree] = useState("");
+  const [budget, setBudget] = useState("");
 
   return (
-
     <>
-    <div className="pricing_budget">
-        <div className="pricingTitle">
-            Pricing Tier:
+      <div className="pricing_budget">
+        <div className="pricingTitle">Pricing Tier:</div>
+
+        <div className="pricingSelect">
+          <select>
+            <option value="tier1">Member</option>
+            <option value="tier2">Non-Member</option>
+          </select>
         </div>
 
-        <div className='pricingSelect'>
-            <select>
-                <option value="tier1">Member</option>
-                <option value="tier2">Non-Member</option>
-            </select>
-        </div>
-
-        <div className="budgetTitle">
-            Budget:
-        </div>
-
-        <input className='budgetVal'/>
-    </div>
-    <div>
-      {/* sugar freee vegan gluten free */}
-      {/* change value to be what db name col is */}
-
-      <header> Dietary Restrictions</header>
-      <div className="">
-        <label for="vegan">Vegan</label>
+        <div className="budgetTitle">Budget:</div>
         <input
-          type="radio"
-          checked={vegan === "vegan"}
-          onChange={() => setVegan("Vegan")}
-        />
-        <br></br>
-
-        <label for="dairy">Dairy Free</label>
-        <input
-          type="radio"
-          checked={dairyFree === "dairyFree"}
-          onChange={() => setDairyFree("dairyFree")}
-        />
-        <br></br>
-
-        <label for="glutenFree">Gluten Free</label>
-        <input
-          type="radio"
-          checked={glutenFree === "glutenFree"}
-          onChange={() => setGlutenFree("glutenFree")}
-        />
-        <br></br>
-        <label for="sugarFree">Sugar Free</label>
-        <input
-          type="radio"
-          checked={sugarFree === "sugarFree"}
-          onChange={() => setSugarFree("sugarFree")}
+          className="budgetVal"
+          placeholder="Budget: "
+          type="text"
+          onChange={(e) => {
+            setBudget(e.currentTarget.value);
+          }}
+          value={budget}
         />
       </div>
-    </div>
+      <div>
+        {/* sugar freee vegan gluten free */}
+        {/* change value to be what db name col is */}
+
+        <header> Dietary Restrictions</header>
+        <div className="">
+          <label for="vegan">Vegan</label>
+          <input
+            type="radio"
+            checked={vegan === "vegan"}
+            onChange={() => setVegan("Vegan")}
+          />
+          <br></br>
+
+          <label for="dairy">Dairy Free</label>
+          <input
+            type="radio"
+            checked={dairyFree === "dairyFree"}
+            onChange={() => setDairyFree("dairyFree")}
+          />
+          <br></br>
+
+          <label for="glutenFree">Gluten Free</label>
+          <input
+            type="radio"
+            checked={glutenFree === "glutenFree"}
+            onChange={() => setGlutenFree("glutenFree")}
+          />
+          <br></br>
+          <label for="sugarFree">Sugar Free</label>
+          <input
+            type="radio"
+            checked={sugarFree === "sugarFree"}
+            onChange={() => setSugarFree("sugarFree")}
+          />
+        </div>
+      </div>
     </>
   );
 };
