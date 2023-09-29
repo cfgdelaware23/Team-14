656ab1fcbbc95df9cc7d93f7140c802d14a6bdbe
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2023-09-29 17:37:10 -0400

    merging two branches for home page

--- crunchkiosk/src/pages/Home.js
+++ crunchkiosk/src/pages/Home.js
@@ -11,6 +11,61 @@ const Home = () => {
   return (
 
     <>
+    <div className="pricing_budget">
+        <div className="pricingTitle">
+            Pricing Tier:
+        </div>
+
+        <div className='pricingSelect'>
+            <select>
+                <option value="tier1">Member</option>
+                <option value="tier2">Non-Member</option>
+            </select>
+        </div>
+
+        <div className="budgetTitle">
+            Budget:
+        </div>
+
+        <input className='budgetVal'/>
+    </div>
+    <div>
+      {/* sugar freee vegan gluten free */}
+      {/* change value to be what db name col is */}
+
+      <header> Dietary Restrictions</header>
+      <div className="">
+        <label for="vegan">Vegan</label>
+        <input
+          type="radio"
+          checked={vegan === "vegan"}
+          onChange={() => setVegan("Vegan")}
+        />
+        <br></br>
+
+        <label for="dairy">Dairy Free</label>
+        <input
+          type="radio"
+          checked={dairyFree === "dairyFree"}
+          onChange={() => setDairyFree("dairyFree")}
+        />
+        <br></br>
+
+        <label for="glutenFree">Gluten Free</label>
+        <input
+          type="radio"
+          checked={glutenFree === "glutenFree"}
+          onChange={() => setGlutenFree("glutenFree")}
+        />
+        <br></br>
+        <label for="sugarFree">Sugar Free</label>
+        <input
+          type="radio"
+          checked={sugarFree === "sugarFree"}
+          onChange={() => setSugarFree("sugarFree")}
+        />
+      </div>
+    </div>
     </>
   );
 };
