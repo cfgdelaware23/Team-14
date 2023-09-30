2704b1efe540e357713cf0c50ff1f2f74baf021f
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2023-09-29 20:46:29 -0400

    add footer to /cart

--- crunchkiosk/src/pages/Cart.js
+++ crunchkiosk/src/pages/Cart.js
@@ -1,10 +1,11 @@
 import CartBox from "../components/CartBox";
+import Footer from "../components/Footer";
 function Cart() {
     return ( 
         <>
             <p>Cart</p>
             <CartBox />
-            {/* <Footer /> */}
+            <Footer />
         </>
     )
 }
