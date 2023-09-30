de5850aa39dd8a454091a37481ee936878bec00b
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2023-09-29 20:24:00 -0400

    footer

--- crunchkiosk/src/components/Footer.js
+++ crunchkiosk/src/components/Footer.js
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../css/Footer.css'
 
 import { FaInstagram, FaTwitter, FaTiktok } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
+    const [total,setTotal] = useState(0);
+    
   return (
     <div className='footer-container'>
         <div className='foot-branding'>
             <span>Code & Design by </span>
             <img src={require('../assets/logo.png')}/>
             <span>Crunch Kiosk</span>
-            <a target="_blank" href="https://www.instagram.com/bmmlatam/">
-                <FaInstagram className='foot-social'/>
-            </a>
+            <Link to="/cart" className='foot-social'>
+                View Cart
+            </Link>
             
         </div>
 
-        <p>crunchkiosk@gmail.com</p>
+        <p>Total: ${total}</p>
 
     </div>
   )
