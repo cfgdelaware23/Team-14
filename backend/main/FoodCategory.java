319ec5eebfa71c18d525ffde2bd43f68c29969b2
Neha <ravuri.n@northeastern.edu>
2023-09-30 01:46:25 -0400

    enumerate enum

--- backend/main/FoodCategory.java
+++ backend/main/FoodCategory.java
@@ -1,5 +1,16 @@
 package main;
 
 public enum FoodCategory {
-    FRUIT, VEGETABLE, CARBOHYDRATE, PROTEIN, DAIRY, BEVERAGE, SNACK;
+    FRUIT(1),
+    VEGETABLE(2),
+    CARBOHYDRATE(3),
+    PROTEIN(4),
+    DAIRY(5),
+    BEVERAGE(6),
+    SNACK(7);
+    int value;
+
+    FoodCategory(int value) {
+        this.value = value;
+    }
 }
