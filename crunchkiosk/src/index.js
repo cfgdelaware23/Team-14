d32e9b5d39a2678d5d93c8c33e066205013c66d5
Abhi <abhijit.das3002@gmail.com>
2023-09-30 00:03:11 -0400

    Backgrounds, integrate cart again

--- crunchkiosk/src/index.js
+++ crunchkiosk/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from './pages/Shop';
 import Selection from './pages/Selection'
-
+import Cart from './pages/Cart'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,6 +16,8 @@ root.render(
       <Route path="/" element={<App />} />
       <Route path="/shop" element={<Shop />} />
       <Route path="/selection" element={<Selection />} />
+      <Route path="/cart" element={<Cart />} />
+
     </Routes>
   </BrowserRouter>
 );
