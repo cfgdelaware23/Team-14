1b4f136453005c0156c8a7b1b891207ceadd150c
Sumedh Kane <33162468+sumedhkane03@users.noreply.github.com>
2023-09-30 01:07:37 -0400

    shop button styles

--- crunchkiosk/src/pages/Shop.js
+++ crunchkiosk/src/pages/Shop.js
@@ -1,6 +1,7 @@
 import "../css/Shop.css";
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import Context from "./Context";
 
@@ -22,9 +23,33 @@ import Context from "./Context";
         ))}
       </div> */}
 function Shop() {
+  const navigate = useNavigate();
+
   const [total, setTotal] = useState(0);
 
   const userData = useContext(Context);
+
+  const goToFruits = () => {
+    navigate("/fruits");
+  };
+
+  const goToVeggies = () => {
+    navigate("/veggies");
+  }
+
+  const goToCarbs = () => {
+    navigate("/carbs");
+  }
+
+  const goToProteins = () => {
+    navigate("/proteins");
+  }
+
+  const goToDairy = () => {
+    navigate("/dairy");
+  }
+
+
 
   return (
     
@@ -33,35 +58,35 @@ function Shop() {
         <p>Shop</p>
       </div>
 
-      <div className="shopItem">
+      <div className="shopItem" onClick={goToFruits}>
         <Link to="/fruits">Fruits</Link>
         <br></br>
         <br></br>
         <p className="subtitle">Choose from a selection of healthy and nutritious fruits!</p>
       </div>
 
-      <div className="shopItem">
+      <div className="shopItem" onClick={goToVeggies}>
         <Link to="/veggies">Veggies</Link>
         <br></br>
         <br></br>
         <p className="subtitle">Choose from a selection of healthy and nutritious vegetables!</p>
       </div>
 
-      <div className="shopItem">
+      <div className="shopItem" onClick={goToCarbs}>
         <Link to="/carbs">Carbs</Link>
         <br></br>
         <br></br>
         <p className="subtitle">Choose from a selection of healthy and nutritious carbohydrates, all based off of your dietary restrictions</p>
       </div>
 
-      <div className="shopItem">
+      <div className="shopItem" onClick={goToProteins}>
         <Link to="/proteins">Proteins</Link>
         <br></br>
         <br></br>
         <p className="subtitle">Choose from a selection of healthy and nutritious proteins, all based off of your dietary restrictions</p>
       </div>
 
-      <div className="shopItem">
+      <div className="shopItem" onClick={goToDairy}>
         <Link to="/dairy">Dairy/Al</Link>
         <br></br>
         <br></br>
